1b5b1f057ad0e0ccb124a6fd0cbc007cb4664030
valeiras <d.r.valeiras@gmail.com>
2023-12-23 06:55:20 +0100

    Uses the github conf

--- app.js
+++ app.js
@@ -1,33 +1,60 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.use(express.json());
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+var app = express();
 
-const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-  res.sendFile('<h1>A node app</h1>');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-// const http = require('http');
 
-// const port = process.env.PORT || 3000;
-
-// http
-//   .createServer(function (request, response) {
-//     response.writeHead(200, { 'Content-Type': 'text/plain' });
-//     response.end('Hello, World!\n');
-//   })
-//   .listen(port);
-
-// console.log(`App is running... (port: ${port})`);
+module.exports = app;
